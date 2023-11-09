78c9b1e768d1ccae5967364b23a5ba4f245e33ab
antonio <antonio@ovex.io>
2023-11-09 10:08:05 +0200

    typscript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import Chat from '../components/Chat'
-import Header from '../components/Header/Header'
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
+import Header from "../components/Header/Header";
 
 const Home = () => {
   return (
     <div>
-      <Header/>
-    <div className='home'>
-      <div className="container">
-        <Sidebar/>
-        <Chat/>
+      <Header />
+      <div className="home">
+        <div className="container">
+          <Sidebar />
+          <Chat />
+        </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
